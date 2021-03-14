fa5b2aa0667fe82bdb4052db9c3c77f6cea012e0
Kelvin Ferreira <10376375+k30v1n@users.noreply.github.com>
2021-03-14 12:24:36 -0300

    adding cors support

--- src/backend/TodoApp/Startup.cs
+++ src/backend/TodoApp/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using TodoApp.Configuration;
 using TodoApp.Data.Database;
 using TodoApp.Service;
 using TodoApp.Store;
@@ -19,9 +20,12 @@ namespace TodoApp
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var todoAppConfiguration = new TodoAppConfiguration();
+            Configuration.Bind(todoAppConfiguration);
+            services.AddSingleton(todoAppConfiguration);
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo
@@ -32,6 +36,14 @@ namespace TodoApp
                 });
             });
 
+            services.AddCors(setup =>
+            {
+                setup.AddDefaultPolicy(policy =>
+                {
+                    policy.WithOrigins(todoAppConfiguration.Cors.AllowedOrigins);
+                });
+            });
+
             services.AddControllers();
             services.AddEntityFrameworkNpgsql().AddDbContext<TodoAppDbContext>();
 
@@ -39,7 +51,6 @@ namespace TodoApp
             services.AddScoped<ITodoStore, TodoStore>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -54,6 +65,7 @@ namespace TodoApp
             }
 
             app.UseRouting();
+            app.UseCors();
 
             app.UseAuthorization();
 
